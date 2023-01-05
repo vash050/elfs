12099a9bc6484ee9b366200441f28ca955f2fecc
vasiliy <vash050@rambler.ru>
2023-01-05 18:52:30 +0300

    -pages -views -requirements.txt

--- app.py
+++ app.py
@@ -1,12 +1,32 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World!'
+def index():
+    return render_template("index.html")
+
+
+@app.route('/list-elfs/')
+def get_list_elfs():
+    return render_template("list_elfs.html")
+
+
+@app.route('/add-elf/')
+def add_elf():
+    return render_template("add_elf.html")
+
+
+@app.route('/about/')
+def about():
+    return render_template("about.html")
+
+
+@app.route('/admin/')
+def admin():
+    return render_template("admin.html")
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
